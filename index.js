475600620d12e6ceb5e1a0c9b2a3b6b2329eb108
Logan Hill <lthill0203@gmail.com>
2024-03-21 23:15:21 -0500

    verifyAdultHood Function

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ const greeting = greet("Logan");
 
 // eslint-disable-next-line no-console
 console.log(greeting);
+
+
+const name = 'Logan';
+const age = 23;
+
+function verifyAdulthood(:'Logan', : 23); {
+  if (age >= 18) {
+    return `Welcome, ${name}!`;
+  } else {
+    return `You are not old enough to enter, ${name}.`;
+  }
+}
+
+
+console.log(verifyAdulthood("Logan", 23));
