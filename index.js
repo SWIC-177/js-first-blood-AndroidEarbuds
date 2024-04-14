e3d8776274b85205d3850df0e50b9e0cc6b2d2cb
Logan Hill <lthill0203@gmail.com>
2024-04-13 21:47:32 -0500

    12-1 part 2 linting errors

--- index.js
+++ index.js
@@ -108,3 +108,50 @@ const person = {
   
   console.log(Person's name is: ${person.name});
   console.log(Other Person's name is: ${otherPerson.name});
+
+
+  // 2nd part of 12-1
+  const people = [
+    {
+      id: 1,
+      name: "John Doe",
+      age: 23,
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      age: 29,
+    },
+    {
+      id: 3,
+      name: "Jim Doe",
+      age: 34,
+    },
+    {
+      id: 4,
+      name: "Jill Doe",
+      age: 45,
+    },
+    {
+      id: 5,
+      name: "Jack Doe",
+      age: 56,
+    },
+    {
+      id: 6,
+      name: "Jenny Doe",
+      age: 67,
+    },
+  ];
+  
+  function renameOdds(peopleList) {
+    for (let i = 0; i < peopleList.length; i+= 1) {
+      if (peopleList[i].id % 2 !== 0) {
+        peopleList[i].name = "Odd Name";
+      }
+    }
+  
+    return p;
+  }
+  
+  console.log(renameOdds(people));
